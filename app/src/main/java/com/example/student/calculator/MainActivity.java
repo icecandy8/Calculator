50d2848c721e2500fde9c38bdad8fc173913e418
icecandy8 <kuber.nandwani@learner.manipal.edu>
2018-03-28 07:24:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/student/calculator/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.student.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+
+    private char CURRENT_ACTION;
+
+    //decimalFormat = new DecimalFormat("#.##########");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
